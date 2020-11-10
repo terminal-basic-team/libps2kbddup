f5c9c5a79d3d8e8e00e14c172170a15a7e951704
starling13 <starling13@mail.ru>
2020-11-10 16:11:08 +0300

    Fix example, remove unused examples

--- ps2kbd/examples/serialecho/serialecho.ino
+++ ps2kbd/examples/serialecho/serialecho.ino
@@ -1,15 +1,15 @@
-#include "ps2kbd.h"
+#include "ps2_keyboardstream.hpp"
 
 #define DATA_PIN 5
 #define CLK_PIN 2
 
-PS2::Keyboard kbd;
-PS2::KeyboardStream kstream(kbd);
+static PS2::Keyboard kbd;
+static PS2::KeyboardStream kstream(kbd);
 
 void
 setup()
 {
-  Serial.begin(115200);
+  Serial.begin(9600);
   kbd.begin(DATA_PIN, CLK_PIN);
   
 }
